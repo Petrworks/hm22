e3320b0fcef6d68a95a19fe58678c6f40ffae25f
Petrworks <pepjje@gmail.com>
2023-01-15 18:44:05 +0200

    made task 2

--- homework4/js/script.js
+++ homework4/js/script.js
@@ -18,3 +18,32 @@ switch (busNum) {
     console.log(`\"Please wait\"`);
     break;
 }
+
+/* 2) Напишіть програму, яка отримує від користувача число і порівнює його з числом Pi. (Math.PI)
+
+Порівняйте, чи є це число більше ніж число Pi.
+Порівняйте, чи є це число менше ніж число Pi.
+Порівняйте, чи це число дорівнює числу Pi.
+Якщо введене значення не є числове, виведіть в консоль повідомлення про помилку.
+Всі результати відобразіть в наступному форматі:
+
+You entered: <number> 
+Is greater then PI: true
+Is less then PI: false
+Is equal PI: false    */
+
+const numPi = prompt();
+
+if (numPi > Math.PI) {
+  console.log(`You entered: ${numPi} 
+  It's greater then PI: true`);
+} else if (numPi < Math.PI) {
+  console.log(`You entered: ${numPi} 
+  It's less then PI: false`);
+} else if (numPi < Math.PI) {
+  console.log(`You entered: ${numPi} 
+  It's equal PI: false`);
+} else if (isNaN) {
+  console.log(`You entered: ${numPi} 
+  It's a wrong data`);
+}
