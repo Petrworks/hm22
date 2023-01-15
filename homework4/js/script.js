3526b19f38d0dbe168d57aeb42782ddef0f55de8
Petrworks <pepjje@gmail.com>
2023-01-15 18:05:47 +0200

    made task 1

--- homework4/js/script.js
+++ homework4/js/script.js
@@ -1,4 +1,20 @@
 /* 1) Напишіть програму, яка питає у користувача номер автобуса.
 
 Якщо це номер 7, 255 или 115, тоді користувач може їхати. Виведіть в консолі "You can go".
-Якщо ні - виведіть в консолі "Please wait".
+Якщо ні - виведіть в консолі "Please wait".*/
+
+const busNum = +prompt();
+switch (busNum) {
+  case 7:
+    console.log(`\"You can go\"`);
+    break;
+  case 115:
+    console.log(`\"You can go\"`);
+    break;
+  case 255:
+    console.log(`\"You can go\"`);
+    break;
+  default:
+    console.log(`\"Please wait\"`);
+    break;
+}
